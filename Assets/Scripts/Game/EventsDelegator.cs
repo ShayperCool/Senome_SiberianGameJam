394f61dcd30e066b3f478a8ce7bf195b695596f8
Alexey Ivanov <shaypercool@mail.ru>
2020-05-01 17:35:40 +0800

    Base

---
+++ Assets/Scripts/Game/EventsDelegator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Game.Models;
+using UnityEngine;
+
+namespace Game {
+	public class EventsDelegator : MonoBehaviour {
+
+		public Action<Note> target;
+
+		public void OnNote(Note note) {
+			target?.Invoke(note);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/Game/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Models {
+	[CreateAssetMenu(fileName = "New Note", menuName = "Create Note", order = 0)]
+	public class Note : ScriptableObject {
+		public int pointsCount = 0;
+		public Sprite sprite;
+		public float duration;
+	}
+}
