ca6860310f0342b4eb05670d0d5a9f57e9566309
ezheka <60533956+ezheka@users.noreply.github.com>
2020-05-01 16:26:31 +0500

    create scene ZHEKA

---
+++ Assets/Scripts/DeleteNote.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteNote : MonoBehaviour
+{
+    public KeyCode keyToDelete = KeyCode.Q;
+    private Collider2D _collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToDelete))
+        {
+            if (_collider!=null)
+            {
+                Destroy(_collider.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "notes")
+        {
+            _collider = collision;
+        }
+    }
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "notes" && Input.GetKeyDown(KeyCode.Q))
+    //    {
+    //        Destroy(collision.gameObject);
+    //    }
+    //}
+    
+
+}
---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject[] lines;
+    public GameObject note;
+    public float Time;
+    public bool Stop;
+
+    public static bool isDelete;
+    void Start()
+    {
+        StartCoroutine(Spawn());
+        isDelete = false;
+    }
+
+    IEnumerator Spawn()
+    {
+        while (!Stop)
+        {
+            int idLine = Random.Range(0, lines.Length);
+            GameObject _note = Instantiate(note, note.transform.position = new Vector2(lines[idLine].transform.localPosition.x, 350f), Quaternion.identity) as GameObject;
+            _note.transform.SetParent(gameObject.transform, false);
+            yield return new WaitForSeconds(Time);
+        }
+    }
+
+}
