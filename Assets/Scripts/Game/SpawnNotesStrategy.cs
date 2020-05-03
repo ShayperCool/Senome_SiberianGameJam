2aaf8bdb2805e81da1e5eead8041e72d5c790699
Alexey Ivanov <shaypercool@mail.ru>
2020-05-04 03:14:24 +0800

    LineHold

--- Assets/Scripts/Game/SpawnNotesStrategy.cs
+++ Assets/Scripts/Game/SpawnNotesStrategy.cs
@@ -1,4 +1,8 @@
-﻿using Game.Models;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Game.Models;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +13,8 @@ namespace Game {
 		public GameObject[] lines;
 		public GameObject notePrefab;
 		public Sprite[] spritesNote;
-
+		private List<int> _linesIds = new List<int>{0, 1, 2, 3, 4, 5};
+		
 		private void Start() {
 			EventsDelegator.Singleton.target = ProcessNote;
 			notePrefab.GetComponent<Image>().sprite = spritesNote[0];
@@ -23,12 +28,19 @@ namespace Game {
 				notePrefab.GetComponent<Image>().sprite = spritesNote[2];
 			if (note.name == "Drum") 
 				notePrefab.GetComponent<Image>().sprite = spritesNote[1];
-			int idLine = Random.Range(0, lines.Length);
+			int idLine = _linesIds[Random.Range(0, lines.Length)];
+			_linesIds.Remove(idLine);
+			FreeLine(idLine);
 			//int idTrack = GameManager.Singleton.currentSequenceTrackNote;
 			//int idLine = GameManager.Singleton.sequenceTrackNote[idTrack];
 			GameManager.Singleton.IncreaseCurrentTrack();
 			GameObject _note = Instantiate(notePrefab, notePrefab.transform.position = new Vector2(lines[idLine].transform.localPosition.x, 330f), Quaternion.identity) as GameObject;
 			_note.transform.SetParent(canvas.transform, false);
+		}
+		
+		private async void FreeLine(int index){
+			await Task.Delay(100);
+			_linesIds.Add(index);
 		}
 	}
 }
